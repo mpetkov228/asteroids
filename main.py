ea7e415ddcbb6390dcf71bc2bb99014ca338b146
mpetkov228 <mpetkov228@gmail.com>
2025-02-09 14:55:31 +0200

    Lock framerate at 60fps

--- main.py
+++ main.py
@@ -2,15 +2,23 @@ import pygame
 from constants import *
 
 def main():
-    pygame.init()
     print("Starting asteroids!")
+
+    pygame.init()
+
+    clock = pygame.time.Clock()
+    dt = 0
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
         screen.fill("black")
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
