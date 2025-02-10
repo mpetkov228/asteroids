945f036c2c7fa8a718e318795f6ae8f4c17fa769
mpetkov228 <mpetkov228@gmail.com>
2025-02-10 14:04:32 +0200

    Create Asteroid class

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
 
 def main():
     print("Starting asteroids!")
@@ -8,8 +9,15 @@ def main():
     pygame.init()
 
     clock = pygame.time.Clock()
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
@@ -19,9 +27,11 @@ def main():
             
         screen.fill("black")
 
-        player.update(dt)
-        player.draw(screen)
-        
+        for obj in updatable:
+            obj.update(dt)
+        for obj in drawable:   
+            obj.draw(screen)
+
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
